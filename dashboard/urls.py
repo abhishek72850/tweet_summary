998cce887670a21d2e6c284d647852e59b8bc2dd
Abhishek kumar <alexkay72850@gmail.com>
2020-09-10 17:20:12 +0530

    deleted v1

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('dashboard', views.dashboard),
-    path('panel', views.choice_panel),
-    path('portal', views.choice_portal)
-]
