5fd6c65960d93a463a66d55d82750d436d0e9c99
Abhishek Kumar <abhishekk728@yahoo.com>
2020-10-21 11:50:15 +0530

    minor

--- tweet_summary/settings.py
+++ tweet_summary/settings.py
@@ -254,8 +254,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'staticfiles')
