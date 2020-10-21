39bcf1dfcdbcb5c7cb725a44d38011032cda2f6a
Abhishek Kumar <abhishekk728@yahoo.com>
2020-10-21 12:44:32 +0530

    minor 1

--- tweet_summary/settings.py
+++ tweet_summary/settings.py
@@ -257,8 +257,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app_ui')
+]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
