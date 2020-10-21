8715463a915dc41f2b7629a26b05bbdd6e025a17
Abhishek Kumar <abhishekk728@yahoo.com>
2020-10-21 12:57:16 +0530

    minor 1

--- tweet_summary/settings.py
+++ tweet_summary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 
 from django.http import HttpRequest
@@ -262,3 +263,4 @@ STATIC_URL = '/static/'
 # ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
