d9ecf7256b399084ca5e174a61976cb622c8fca5
Abhishek Kumar <abhishekk728@yahoo.com>
2020-10-22 18:03:44 +0530

    minor

--- tweet_summary/settings.py
+++ tweet_summary/settings.py
@@ -209,8 +209,8 @@ LOGGING = {
 }
 
 # CELERY STUFF
-CELERY_BROKER_URL = os.environ['REDIS_URL']
-CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 # CELERY_BROKER_URL = 'redis://localhost:6379'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
